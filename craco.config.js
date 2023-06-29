e14e5e62d1d7617fb1252a3bf9effbcd60ebf9ba
heruns <hhccrr@foxmail.com>
2023-06-29 23:19:13 +0800

    add comment

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,8 @@ module.exports = {
   style: {
     sass: {
       loaderOptions: {
-        // 全局添加 scss 前缀代码: https://github.com/webpack-contrib/sass-loader#additionaldata
+        // 全局添加 scss 前缀代码，自动引入 function.scss
+        // 关于 additionalData: https://github.com/webpack-contrib/sass-loader#additionaldata
         additionalData: (content, loaderContext) => {
           const { resourcePath, rootContext } = loaderContext
           const relativePath = path.relative(rootContext, resourcePath)
@@ -26,6 +27,7 @@ module.exports = {
   },
   webpack: {
     configure: (webpackConfig) => {
+      // 配置 src 目录别名
       webpackConfig.resolve = webpackConfig.resolve || {}
       webpackConfig.resolve.alias = webpackConfig.resolve.alias || {}
       webpackConfig.resolve.alias['@'] = path.resolve(__dirname, 'src')
